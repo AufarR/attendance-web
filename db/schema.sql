94a912cc03be6c9153da934a747145262b1f1e85
AufarR <aufaralazhar@gmail.com>
2025-06-19 13:45:58 +0700

    Fix typo

--- db/schema.sql
+++ db/schema.sql
@@ -41,10 +41,10 @@ CREATE TABLE meeting_attendees (
 -- Rooms
 INSERT INTO rooms (name, description, device_name, public_key) VALUES
 ('7601', 'Labtek 5 lt. 1', 'RoomAService', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCCXU37nqdTtejm7he2N\nr6Me9M++PGVGHhbwwswZMKZ5m74K7zTpMoXyEcuHdCFHAZPJYkwDtReEM935NqvS\n+ST9vToEiXuAT0VpnIZ1KaxPQBMU/nzPbibW49phIdVM3oHDbcMaYWAJG5a+7og8\nkXtDAHQDyqfnUNHrQ38CWYlxH49dWKSdxmtEOEszru9vfpM24VT+44Kd38g3HWrL\nOzZNXll/5fNEs8aSRPf2/m8B8toUdP1weyCKxMrcFMEC+B9oyKWUcLIbidfZgyCn\nrqSWbPh2um19xLr2ivvQTcNoV2I+Ey9c3peCRfXCUckRLyR6HoXU2kePt4kmCVql\nBwIDAQAB\n-----END PUBLIC KEY-----'),
-('Multimedia', 'Labtek 5 lt. 3', 'RoomBService', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp1jx7n++vJ4uX+4v4HbF\nnUI3tiPKuJApQios3LmGxW61pA9YsniwrVDb4TmcEscy+0thd2vy6DvVcWmcrKC9\nndtRlWdsNKhgmT4PllBego8fmK8Kv9ZWUQNXSwDTpmTNW1BXZ+6SNML+XGWxcoZg\n3kTQ0aBFPBmAR1ZKarXpBtNtk3qzMBp0S5fCBDmltttfAwnYnJD9PIUvDqZl2/XY\nnPbXc3Jb229rbAHNqgQckhcqaNbif6MCy76++IQgLwq45PsGV24StU2tY0a8ypVc\nhw6Ht00HoCmY3oe28x/TKR1vbydOxg/VHeOq+x9I1Db21Wpdgv34i2CUbvbCjOE7\nCQIDAQAB\n-----END PUBLIC KEY-----')
+('Multimedia', 'Labtek 5 lt. 3', 'RoomBService', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp1jx7n++vJ4uX+4v4HbF\nnUI3tiPKuJApQios3LmGxW61pA9YsniwrVDb4TmcEscy+0thd2vy6DvVcWmcrKC9\nndtRlWdsNKhgmT4PllBego8fmK8Kv9ZWUQNXSwDTpmTNW1BXZ+6SNML+XGWxcoZg\n3kTQ0aBFPBmAR1ZKarXpBtNtk3qzMBp0S5fCBDmltttfAwnYnJD9PIUvDqZl2/XY\nnPbXc3Jb229rbAHNqgQckhcqaNbif6MCy76++IQgLwq45PsGV24StU2tY0a8ypVc\nhw6Ht00HoCmY3oe28x/TKR1vbydOxg/VHeOq+x9I1Db21Wpdgv34i2CUbvbCjOE7\nCQIDAQAB\n-----END PUBLIC KEY-----');
 
 -- Users (Passwords will be hashed using argon2id)
 -- Note: The passwords below are set as "password" and should be replaced with stronger values in production.
 INSERT INTO users (name, email, password, role) VALUES
 ('Alice', 'alice@example.com', '$argon2id$v=19$m=8192,t=2,p=1$N2c0QTFHdHhQdEE3N3RRYw$AAVd9Cg6d7ge254LU+/PeQ', 'host'), 
-('Bob', 'bob@example.com', '$argon2id$v=19$m=8192,t=2,p=1$RUdUNUxMdDdsZzliNFpRVw$Y/4shGIRJpbVYnYObGeLoQ', 'attendee')
+('Bob', 'bob@example.com', '$argon2id$v=19$m=8192,t=2,p=1$RUdUNUxMdDdsZzliNFpRVw$Y/4shGIRJpbVYnYObGeLoQ', 'attendee');
